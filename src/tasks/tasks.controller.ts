e6f86e0482e847ef31ebc1373c2650abab58bbef
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2022-04-18 15:40:46 +0500

    adding get tasks with filter

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -6,9 +6,10 @@ import {
   Param,
   Delete,
   Patch,
+  Query,
 } from '@nestjs/common';
-import { title } from 'process';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { Task, TaskStatus } from './task.model';
 import { TasksService } from './tasks.service';
 
@@ -17,7 +18,12 @@ export class TasksController {
   constructor(private taskService: TasksService) {}
 
   @Get()
-  getAllTasks(): Task[] {
+  getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+    if (Object.keys(filterDto).length) {
+      return this.taskService.getTasksWithFilters(filterDto);
+    } else {
+      return this.taskService.getAllTasks();
+    }
     return this.taskService.getAllTasks();
   }
 
